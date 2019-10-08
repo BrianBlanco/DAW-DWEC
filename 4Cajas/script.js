b9084a356ba2cd92a5cb432db9a8a27c8b01302b
BRIAN GIANNONI LILLO <briangiannoni@gmail.com>
2019-10-08 19:50:23 +0200

    cajas acabadas

--- 4Cajas/script.js
+++ 4Cajas/script.js
@@ -1,21 +1,32 @@
+var cambiarRojo = true;
+var cambiarAzul = true;
+var cambiarVerde = true;
+var cambiarNegro = true;
+
 // Redondear el cuadrado cuando se le apunta
 function alApuntarAlDiv(cuadradoARedondear) {
 
-    // Redondear el cuadrado
-    cuadradoARedondear.style.borderRadius = "50%";
+        // Redondear el cuadrado
+        cuadradoARedondear.style.borderRadius = "50%";
 
-    // Tiempo de transición
-    cuadradoARedondear.style.transitionDuration = "1s";
+        // Tiempo de transición
+        cuadradoARedondear.style.transitionDuration = "1s";
 }
 
 // Desredondear el cuadrado cuando se deja de apuntar
-function alNoApuntarAlDiv(cuadrdoADesredondear) {
+function alNoApuntarAlDiv(cuadradoADesredondear) {
+        if (mantenerEstatico(cuadradoADesredondear)) {
+            
+        // Redondear el cuadrado
+        cuadradoADesredondear.style.borderRadius = "0%";
 
-    // Redondear el cuadrado
-    cuadrdoADesredondear.style.borderRadius = "0%";
+        // Tiempo de transición
+        cuadradoADesredondear.style.transitionDuration = "1s";
 
-    // Tiempo de transición
-    cuadrdoADesredondear.style.transitionDuration = "1s";
+        // Volver a poner la sombra
+        cuadradoADesredondear.style.boxShadow = "5px -2px 1px #000";
+    }
+
 }
 
 // Eliminar cuadrado seleccionado
@@ -27,18 +38,82 @@ function eliminarCuadrado(colorCuadrado, botonAEliminar) {
     // Seleccionamos padre del cuadrado seleccionado
     var padreCuadrados = document.getElementById("cuadros");
 
-    // Eliminamos el cuadrado
-    padreCuadrados.removeChild(elementoCuadrado[0]);
+    elementoCuadrado[0].style.transition = "1s";
+    elementoCuadrado[0].style.width = "0px";
+    setTimeout(function(){ 
 
-    //Eliminar respectivo botón
-    botonAEliminar.parentNode.removeChild(botonAEliminar);
+        // Eliminamos el cuadrado
+        padreCuadrados.removeChild(elementoCuadrado[0]);
+        
+        //Eliminar respectivo botón
+        botonAEliminar.parentNode.removeChild(botonAEliminar);
+    }, 1300);
 }
 
 // Quitar sombra al hacer un click sobre el cuadrado
-function cambiarSombra(cuadrado) {
-    cuadrado.style.boxShadow = "0px 0px 0px";
-    var classDelCuadrado = cuadrado.getAttribute("class");
-    alert(classDelCuadrado);
-    const element = document.querySelector(classDelCuadrado.split(" ")[1])
-    alert(element);
+function quitarSombra(cuadrado) {
+    if (mantenerEstatico(cuadrado)) {
+        cuadrado.style.boxShadow = "0px 0px 0px";
+    }
+}
+
+// Poner sombra interior al pulsar doble click y dejar estático el elemento
+function dobleClick(cuadrado) {
+    cuadrado.style.webkitBoxShadow = "inset -12px -15px 4px -1px rgba(0,0,0,0.75)";
+    cuadrado.style.mozBoxShadow = "inset -12px -15px 4px -1px rgba(0,0,0,0.75)";
+    cuadrado.style.boxShadow = "inset -12px -15px 4px -1px rgba(0,0,0,0.75)";
+
+    var classDelCuadrado = cuadrado.getAttribute("class").split(" ")[1];
+
+    switch (classDelCuadrado) {
+        case "rojo":
+            cambiarRojo = false;
+            break;
+        case "azul":
+            cambiarAzul = false;
+           break;
+        case "verde":
+            cambiarVerde = false;
+          break;
+      
+        case "negro":
+            cambiarNegro = false;
+          break;
+
+        default:
+            break;
+    }
+    
+}
+
+function mantenerEstatico(cuadrado) {
+    var cambiarlo = false;
+
+    switch (cuadrado.getAttribute("class").split(" ")[1]) {                
+        case "rojo":
+            if (cambiarRojo) {
+                cambiarlo = true;
+            }
+            break;
+        case "azul":
+            if (cambiarAzul) {
+                cambiarlo = true;
+            }
+           break;
+        case "verde":
+            if (cambiarVerde) {
+                cambiarlo = true;
+            }
+          break;
+      
+        case "negro":
+            if (cambiarNegro) {
+                cambiarlo = true;
+            }
+          break;
+
+        default:
+            break;
+    }
+    return cambiarlo;
 }
